ea7d96a5e95c997a65bf768fce6263d6469fd291
Volker Buzek <dev@metalodge.com>
2025-03-28 20:48:53 +0100

    feat: sha check, git clone

--- commands/modules/common.ts
+++ commands/modules/common.ts
@@ -1,4 +1,5 @@
 import { Spinner } from "@std/cli/unstable-spinner"
+import { createHash } from "https://deno.land/std@0.203.0/hash/mod.ts"
 import process from "node:process"
 
 export function progress() {
@@ -8,8 +9,8 @@ export function progress() {
   const _stop = () => {
     originalStop.call(spinner)
     // hacky hacksor to remove the "setting up csap ..." message
-    process.stdout.moveCursor(0, -1) 
-    process.stdout.clearLine(1) 
+    process.stdout.moveCursor(0, -1)
+    process.stdout.clearLine(1)
   }
   spinner.stop = _stop
   return spinner
@@ -57,5 +58,63 @@ export const sapIntegrationModules = [
     description: "Configure all modules",
   },
 ]
-
 export type integrationModule = typeof sapIntegrationModules[number]
+
+export async function compareFilesBySha256(
+  filePath1: string,
+  filePath2: string,
+): Promise<boolean> {
+  const hashFile = async (filePath: string): Promise<string> => {
+    const fileContent = await Deno.readFile(filePath)
+    const hash = createHash("sha256")
+    hash.update(fileContent)
+    return hash.toString()
+  }
+
+  const [hash1, hash2] = await Promise.all([
+    hashFile(filePath1),
+    hashFile(filePath2),
+  ])
+  return hash1 === hash2
+}
+
+export async function getGitCommitHash(
+  directory: string,
+): Promise<string | null> {
+  try {
+    const process = new Deno.Command("git", {
+      args: ["-C", directory, "rev-parse", "HEAD"],
+      stdout: "piped",
+      stderr: "piped",
+    })
+
+    const { stdout, stderr } = await process.output()
+
+    if (stderr.length > 0) {
+      console.error(new TextDecoder().decode(stderr))
+      return null
+    }
+
+    return new TextDecoder().decode(stdout).trim()
+  } catch (err) {
+    console.error("Error retrieving git commit hash:", err)
+    return null
+  }
+}
+
+export async function clone(repo: string, branch: string, to: string) {
+  const process = new Deno.Command("git", {
+    args: ["clone", "--branch", branch, repo, to],
+    stdout: "piped",
+    stderr: "piped",
+  })
+
+  const { stdout, stderr } = await process.output()
+
+  if (stderr.length > 0) {
+    console.error(new TextDecoder().decode(stderr))
+    return null
+  }
+
+  return new TextDecoder().decode(stdout)
+}
