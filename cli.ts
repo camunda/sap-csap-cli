3cbb5b85258cb9461696c2028334a34e791002ef
Volker Buzek <dev@metalodge.com>
2025-03-28 13:38:24 +0100

    refactor: design req

--- cli.ts
+++ cli.ts
@@ -2,47 +2,31 @@ import yargs from "https://deno.land/x/yargs/deno.ts"
 import { setupCommand } from "./commands/setup.ts"
 
 console.log(
-  "\nWelcome to %cCamunda's %cSAP %cIntegration CLI (csap)!",
-  "color: orange",
-  "color: blue",
-  "color: auto",
+  "Welcome to csap - Camunda’s SAP Integration CLI",
 )
+console.log("https://docs.camunda.io/docs/components/camunda-integrations/sap/csap-cli.md")
 console.log(
-  `
-%c  *@#%       %c  
-%c :@* :@      %c/////////////////////     
-%c :@*         %c(    ((  ((     (((        
-%c :@*         %c(    (    (               
-%c :@* :@      %c#      ##    #            
-%c  #@*%       %c###########     
-                
-%c :=====:         
- :=====:         
-`,
-  "color:orange",
-  "color: blue",
-  "color:orange",
-  "color: blue",
-  "color:orange",
-  "color: blue",
-  "color:orange",
-  "color: blue",
-  "color:orange",
-  "color: blue",
-  "color:orange",
-  "color: blue",
-  "color: auto",
+` 
+  __      __   _   ___ 
+ / _| __ / _| / \\ | o \\
+| |_  __ \\_ \\| o ||  _/
+ \\__|    |__/|_n_||_|  
+                                 
+`
 )
 
 yargs(Deno.args)
   .scriptName("csap")
+  .usage("Usage: \n  $0 <command>")
   .version(
     (await import("./deno.json", {
       with: { type: "json" },
     })).default.version,
   )
+  .alias("version", "v")
   .command(setupCommand)
-  .demandCommand(1, "You need at least one command before moving on")
+  .demandCommand(1, "Choose a command to continue (Ex: csap setup)")
   .strict()
   .help()
+  .alias("help", "h")
   .parse()
