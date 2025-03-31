3de00c78185e5a466dd71462a592df83264c4c2d
Volker Buzek <dev@metalodge.com>
2025-03-31 20:20:57 +0200

    refactor: outsource setup handler fn

---
+++ commands/modules/setup-handler.ts
@@ -0,0 +1,132 @@
+import { Ask } from "@sallai/ask"
+const ask = new Ask()
+
+import { btpPlugin } from "./btp_plugin.ts"
+import { odataConnector } from "./odata_connector.ts"
+import { rfcConnector } from "./rfc_connector.ts"
+import {
+  camundaDeploymentOptions,
+  camundaVersions,
+  sapIntegrationModules,
+} from "./common.ts"
+import {
+  detectCredentials,
+  getCredentials,
+  getCredentialsFromEnv,
+} from "./credentials.ts"
+
+export async function setupHandler(argv) {
+  const sapIntegrationModule = argv.for ||
+    (await ask.select({
+      name: "for",
+      message: "SAP integration module",
+      default: sapIntegrationModules[1].value,
+      choices: sapIntegrationModules,
+    })).for
+  const camundaVersion = argv.camunda ||
+    (await ask.select({
+      name: "camunda",
+      message: "Camunda version",
+      default: camundaVersions[0].value,
+      choices: camundaVersions,
+    })).camunda
+  const camundaDeployment = argv.deployment ||
+    (await ask.select({
+      name: "deployment",
+      message: "Deployment option",
+      default: camundaDeploymentOptions[0].value,
+      choices: camundaDeploymentOptions,
+    })).deployment
+  const btpRoute = sapIntegrationModule === sapIntegrationModules.find(
+      (m) => m.value === "btp-plugin",
+    )?.value
+    ? argv.btpRoute ||
+      (await ask.input({
+        name: "btpRoute",
+        message:
+          "BTP route to reach the plugin\n (host name only!, e.g. camunda-app)",
+        validate: (input: string) => {
+          if (input.includes("hana.ondemand")) {
+            return false
+          } else {
+            return true
+          }
+        },
+        default: "camunda-btp-plugin",
+      })).btpRoute
+    : "n/a"
+
+  let clusterId, region, clientId, clientSecret
+  if (
+    argv.clusterId && argv.region && argv.clientId &&
+    argv.clientSecret
+  ) {
+    ;({ clusterId, region, clientId, clientSecret } = argv)
+  } else if (detectCredentials() === true) {
+    ;({
+      CAMUNDA_CLUSTER_ID: clusterId,
+      CAMUNDA_CLUSTER_REGION: region,
+      CAMUNDA_CLIENT_ID: clientId,
+      CAMUNDA_CLIENT_SECRET: clientSecret,
+    } = getCredentialsFromEnv())
+    console.log("\ni Camunda API credentials found in environment:")
+    const log = Object.entries(getCredentialsFromEnv()).reduce(
+      (acc, [key, value]) => {
+        acc[key] = "***" + value.slice(-3)
+        return acc
+      },
+      {} as Record<string, string>,
+    )
+    console.table(log)
+  } else {
+    console.log(
+      "\ni No Camunda API credentials found in environment. Enter credentials manually",
+    )
+    ;({ clusterId, region, clientId, clientSecret } = await getCredentials(
+      argv,
+    ))
+  }
+  const credentials = {
+    clusterId,
+    region,
+    clientId,
+    clientSecret,
+  }
+
+  switch (sapIntegrationModule) {
+    case "btp-plugin":
+      await btpPlugin({
+        camundaVersion,
+        camundaDeployment,
+        credentials,
+        btpRoute,
+      })
+      break
+    case "odata":
+      odataConnector()
+      break
+    case "rfc":
+      rfcConnector()
+      break
+    default:
+      console.error("invalid SAP integration module selected")
+      Deno.exit(1)
+  }
+
+  console.log(
+    `\n%c✔ Setup completed successfully`,
+    "color: green",
+  )
+
+  // FIXME: delete this
+  //   console.debug(argv, {
+  //     sapIntegrationModule,
+  //     camundaVersion,
+  //     camundaDeployment,
+  //     btpRoute,
+  //     clusterId,
+  //     region,
+  //     clientId,
+  //     clientSecret,
+  //   })
+}
--- commands/setup.ts
+++ commands/setup.ts
@@ -12,7 +12,7 @@ import {
   getCredentialsFromEnv,
   sapIntegrationModules,
 } from "./modules/common.ts"
-import { YargsInstance } from "https://deno.land/x/yargs@v17.7.2-deno/build/lib/yargs-factory.js"
+import { setupHandler } from "./modules/setup-handler.ts"
 
 export const setupCommand = {
   command:
@@ -59,111 +59,5 @@ export const setupCommand = {
         description: "Camunda API Client: OAuth2 client secret",
       })
   },
-  handler: async (argv) => {
-    const sapIntegrationModule = argv.for ||
-      (await ask.select({
-        name: "for",
-        message: "SAP integration module",
-        default: sapIntegrationModules[1].value,
-        // REVISIT: this has usability issues on smaller terminals
-        // choices: sapIntegrationModules.map(m => ({ message: m.message + "\t" + m.description, value: m.value })),
-        choices: sapIntegrationModules,
-      })).for
-    const camundaVersion = argv.camunda ||
-      (await ask.select({
-        name: "camunda",
-        message: "Camunda version",
-        default: camundaVersions[0].value,
-        choices: camundaVersions,
-      })).camunda //> b/c .select() returns an object like { $name: "8.7" }
-    const camundaDeployment = argv.deployment ||
-      (await ask.select({
-        name: "deployment",
-        message: "Deployment option",
-        default: camundaDeploymentOptions[0].value,
-        choices: camundaDeploymentOptions,
-      })).deployment
-    // this option is only relevant, when
-    // the selected SAP integration module is "btp-plugin"
-    const btpRoute = sapIntegrationModule === sapIntegrationModules.find(
-        (m) => m.value === "btp-plugin",
-      )?.value
-      ? argv.btpRoute ||
-        (await ask.input({
-          name: "btpRoute",
-          message:
-            "BTP route to reach the plugin\n (host name only!, e.g. camunda-app)",
-          validate: (input: string) => {
-            if (input.includes("hana.ondemand")) {
-              return false
-            } else {
-              return true
-            }
-          },
-          default: "camunda-btp-plugin",
-        })).btpRoute
-      : "n/a"
-
-    // credential logic
-    // 1. cmd line > 2. env > 3. ask
-    let clusterId, region, clientId, clientSecret
-    if (
-      argv.clusterId && argv.region && argv.clientId &&
-      argv.clientSecret
-    ) {
-      ;({ clusterId, region, clientId, clientSecret } = argv)
-    } else if (detectCredentials() === true) {
-      ;({
-        CAMUNDA_CLUSTER_ID: clusterId,
-        CAMUNDA_CLUSTER_REGION: region,
-        CAMUNDA_CLIENT_ID: clientId,
-        CAMUNDA_CLIENT_SECRET: clientSecret,
-      } = getCredentialsFromEnv())
-      console.log("\ni Camunda API credentials found in environment:")
-      const log = Object.entries(getCredentialsFromEnv()).reduce(
-        (acc, [key, value]) => {
-          acc[key] = "***" + value.slice(-3)
-          return acc
-        },
-        {} as Record<string, string>,
-      )
-      console.table(log)
-    } else {
-      console.log("\ni No Camunda API credentials found in environment. Enter credentials manually")
-      ;({ clusterId, region, clientId, clientSecret } = await getCredentials(
-        argv,
-      ))
-    }
-    const credentials = {
-      clusterId,
-      region,
-      clientId,
-      clientSecret,
-    }
-
-    switch (sapIntegrationModule) {
-      case "btp-plugin":
-        await btpPlugin({
-          camundaVersion,
-          camundaDeployment,
-          credentials,
-          btpRoute,
-        })
-        break
-      case "odata":
-        odataConnector()
-        break
-      case "rfc":
-        rfcConnector()
-        break
-      default:
-        console.error("invalid SAP integration module selected")
-        Deno.exit(1)
-    }
-
-    console.log(
-      `\n%c✔ Setup completed successfully`,
-      "color: green",
-    )
-  },
+  handler: setupHandler,
 }
