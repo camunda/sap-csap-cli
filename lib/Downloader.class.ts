5d88fdf18d00ed8c8ea4f5c7e2ab45fa296bcb9e
Volker Buzek <dev@metalodge.com>
2025-04-01 12:06:43 +0200

    wip: downloading releases

--- lib/Downloader.class.ts
+++ lib/Downloader.class.ts
@@ -1,7 +1,73 @@
-export class Downloader {
-    private to: string;
+import { GetResponseTypeFromEndpointMethod } from "npm:@octokit/types"
+import { Octokit } from "https://esm.sh/octokit?dts"
 
-    constructor() {
-        this.to = `${Deno.env.get('TMPDIR') || '/tmp'}/camunda`;
+const octokit = new Octokit({
+  userAgent: "csap",
+})
+
+export class Downloader {
+  private to: string
+  public static Kind = {
+    odata: "sap-odata-connector",
+    rfc: "sap-rfc-connector",
+    btp: "sap-btp-plugin",
+  } as const
+  releases: Array<
+    GetResponseTypeFromEndpointMethod<typeof octokit.request>["data"][number]
+  > = []
+  for: {
+    module: (typeof Downloader.Kind)[keyof typeof Downloader.Kind]
+    version: `${number}.${number}`
+  }
+
+  constructor(
+    module: (typeof Downloader.Kind)[keyof typeof Downloader.Kind],
+    version: `${number}.${number}`, //> c8 release, e.g. 8.7
+  ) {
+    this.to = `${Deno.env.get("TMPDIR") || "/tmp"}/camunda`
+    this.for = {
+      module,
+      version,
     }
+  }
+
+  async getReleases() {
+    const releases = await octokit.request(
+      "GET /repos/{owner}/{repo}/releases",
+      {
+        owner: "camunda",
+        repo: this.for.module,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      },
+    )
+    this.releases = releases.data
+    return this.releases
+  }
+
+  async getLatestRelease() {
+    if (!this.releases.length) {
+      await this.getReleases()
+    }
+    const _releases = this.releases.filter((release) => {
+      const semver = release.name ? release.name : "0.0.0"
+      const [major, minor] = semver.split(".").map(Number)
+      const [targetMajor, targetMinor] = this.for.version.split(".").map(Number)
+
+      return major === targetMajor && minor === targetMinor
+    })
+
+    _releases.sort((a, b) => {
+      const semverA = a.name ? a.name : "0.0.0"
+      const semverB = b.name ? b.name : "0.0.0"
+
+      return semverA.localeCompare(semverB, undefined, {
+        numeric: true,
+        sensitivity: "base",
+      })
+    })
+    const latestRelease = this.releases[this.releases.length - 1]
+    return latestRelease
+  }
 }
---
+++ test/downloader.test.ts
@@ -0,0 +1,27 @@
+import {
+    assertGreaterOrEqual,
+    assertInstanceOf,
+    assertMatch,
+    assertNotEquals
+} from "jsr:@std/assert"
+import { Downloader } from "../lib/Downloader.class.ts"
+
+Deno.test("should return a list of releases", async () => {
+  const releases = await new Downloader(Downloader.Kind.odata, "8.6")
+    .getReleases()
+  assertGreaterOrEqual(releases.length, 1)
+})
+
+Deno.test("should return the latest release", async () => {
+  const downloader = new Downloader(Downloader.Kind.rfc, "8.5")
+  const latestRelease = await downloader.getLatestRelease()
+  assertInstanceOf(latestRelease, Object)
+  assertMatch(
+    latestRelease.name,
+    /8\.5\.\d+/,
+  )
+  assertNotEquals(
+    latestRelease.name,
+    "8.5.0",
+  )
+})
