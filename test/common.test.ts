7536dd43e465b69dd90a898ba7d38ff56d9b040d
Volker Buzek <dev@metalodge.com>
2025-04-04 12:27:39 +0200

    test: cleanup

--- test/common.test.ts
+++ test/common.test.ts
@@ -1,31 +1,5 @@
-import { compareFilesBySha256, getGitCommitHash, compareFilesByName } from "../lib/common.ts"
 import { assertEquals, assertNotEquals } from "jsr:@std/assert"
-
-Deno.test("compareFilesBySha256 - identical files", async () => {
-  const file1 = await Deno.makeTempFile()
-  const file2 = await Deno.makeTempFile()
-  await Deno.writeTextFile(file1, "test content")
-  await Deno.writeTextFile(file2, "test content")
-
-  const result = await compareFilesBySha256(file1, file2)
-  assertEquals(result, true)
-
-  await Deno.remove(file1)
-  await Deno.remove(file2)
-})
-
-Deno.test("compareFilesBySha256 - different files", async () => {
-  const file1 = await Deno.makeTempFile()
-  const file2 = await Deno.makeTempFile()
-  await Deno.writeTextFile(file1, "test content")
-  await Deno.writeTextFile(file2, "different content")
-
-  const result = await compareFilesBySha256(file1, file2)
-  assertEquals(result, false)
-
-  await Deno.remove(file1)
-  await Deno.remove(file2)
-})
+import { getGitCommitHash } from "../lib/common.ts"
 
 Deno.test("getGitCommitHash - valid repository", async () => {
   const tempDir = await Deno.makeTempDir()
@@ -56,29 +30,3 @@ Deno.test("getGitCommitHash - invalid directory", async () => {
   await Deno.remove(tempDir, { recursive: true })
 })
 
-Deno.test("compareFilesByName - identical file names", async () => {
-    const tempDir = await Deno.makeTempDir()
-    const file1 = `${tempDir}/test-file.txt`
-    const file2 = `${tempDir}/test-file.txt`
-
-    await Deno.writeTextFile(file1, "test content")
-
-    const result = await compareFilesByName(file1, file2)
-    assertEquals(result, true)
-
-    await Deno.remove(tempDir, { recursive: true })
-})
-
-Deno.test("compareFilesByName - different file names", async () => {
-    const tempDir = await Deno.makeTempDir()
-    const file1 = `${tempDir}/test-file1.txt`
-    const file2 = `${tempDir}/test-file2.txt`
-
-    await Deno.writeTextFile(file1, "test content")
-    await Deno.writeTextFile(file2, "test content")
-
-    const result = await compareFilesByName(file1, file2)
-    assertEquals(result, false)
-
-    await Deno.remove(tempDir, { recursive: true })
-})
