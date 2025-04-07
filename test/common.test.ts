494cbdd1cce1f109c6200654f9148d314e5c1478
Volker Buzek <dev@metalodge.com>
2025-04-07 12:50:11 +0400

    test: wip fixing git command

--- test/common.test.ts
+++ test/common.test.ts
@@ -6,7 +6,7 @@ Deno.test("getGitCommitHash - valid repository", async () => {
   const gitInit = new Deno.Command("git", { args: ["init", tempDir] })
   const init = await gitInit.output()
   if (!init.success) {
-    console.error("git init failed:", init.stderr)
+    console.error(`git init failed: ${new TextDecoder().decode(init.stderr)}`)
   }
 
   // Create a file and make an initial commit
@@ -15,14 +15,25 @@ Deno.test("getGitCommitHash - valid repository", async () => {
   const gitAdd = new Deno.Command("git", { args: ["-C", tempDir, "add", "."] })
   const add = await gitAdd.output()
   if (!add.success) {
-    console.error("git add failed:", add.stderr)
+    console.error(`git add failed: ${new TextDecoder().decode(add.stderr)}`)
+  }
+  const gitBranch = new Deno.Command("git", {
+    args: ["-C", tempDir, "branch", "-M", "main"],
+  })
+  const branch = await gitBranch.output()
+  if (!branch.success) {
+    console.error(
+      `git branch failed: ${new TextDecoder().decode(branch.stderr)}`,
+    )
   }
   const gitCommit = new Deno.Command("git", {
     args: ["-C", tempDir, "commit", "-m", "Initial commit"],
   })
   const commit = await gitCommit.output()
   if (!commit.success) {
-    console.error("git commit failed:", commit.stderr)
+    console.error(
+      `git commit failed: ${new TextDecoder().decode(commit.stderr)}`,
+    )
   }
 
   const commitHash = await getGitCommitHash(tempDir)
@@ -38,4 +49,3 @@ Deno.test("getGitCommitHash - invalid directory", async () => {
 
   await Deno.remove(tempDir, { recursive: true })
 })
-
